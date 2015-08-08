cd2d43b7ca552c6b1995c79ac2d8e1965c94f09d
Ryan N. Norris <ryan.norris@my.ccsu.edu>
2015-08-08 16:55:20 -0600

    Fixing up some stuff for Carbon.

--- carbon.mk
+++ carbon.mk
@@ -1,10 +1,10 @@
 $(call inherit-product, device/motorola/obake/full_obake.mk)
 
 # Inherit some common Carbon stuff.
-$(call inherit-product, vendor/carbon/config/common_full_phone.mk)
+$(call inherit-product, vendor/carbon/config/common_phone.mk)
 
-# Enhanced NFC
-$(call inherit-product, vendor/carbon/config/nfc_enhanced.mk)
+# Enhanced NFC / Carbon doesn't have this I guess
+# $(call inherit-product, vendor/carbon/config/nfc_enhanced.mk)
 
 PRODUCT_RELEASE_NAME := DROID MAXX
 PRODUCT_NAME := carbon_obake
